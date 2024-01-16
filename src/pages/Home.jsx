8770ea0c05b7a387bfba5e611e04d055e7a885cb
ataha1 <a.taha12322@gmail.com>
2024-01-16 17:35:43 +0200

    Part 19-23

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,10 +2,22 @@ import React from "react";
 import { JumbotronContainer } from "../containers/jumbotron";
 import { FooterContainer } from "../containers/footer";
 import { FaqsContainer } from "../containers/faqs";
+import { HeaderContainer } from "../containers/header";
+import { OptForm } from "../components";
 
 export default function Home() {
   return (
     <div>
+      <HeaderContainer>
+        <OptForm>
+          <OptForm.Input placeholder="Email Address"></OptForm.Input>
+          <OptForm.Button>Try it now</OptForm.Button>
+          <OptForm.Text>
+            Ready to watch? Enter your email to create or restart your
+            membership.
+          </OptForm.Text>
+        </OptForm>
+      </HeaderContainer>
       <JumbotronContainer />
       <FaqsContainer />
       <FooterContainer />
